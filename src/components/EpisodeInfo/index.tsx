24013f222db4127942c1e0784348ab192d2264f9
Augusto Avila <augusto.avila@usemobile.xyz>
2022-01-23 20:12:15 -0300

    feat: add EpisodeInfo component

---
+++ src/components/EpisodeInfo/index.tsx
@@ -0,0 +1,35 @@
+import moment from 'moment';
+
+import { EpisodeInfoDiv } from './styles';
+
+export interface RatingProps {
+  average: number;
+}
+
+export interface EpisodeInfoProps {
+  season: number;
+  number: number;
+  airdate: string;
+  airtime: string;
+  runtime: number;
+  rating: RatingProps;
+}
+
+export const EpisodeInfo = ({
+  season,
+  number,
+  airdate,
+  airtime,
+  runtime,
+  rating,
+}: EpisodeInfoProps) => (
+  <EpisodeInfoDiv>
+    <h2>Episode Info</h2>
+    <p>
+      Number: Season {season}, Episode {number}
+    </p>
+    <p>Airdate: {moment(`${airdate}T${airtime}`).format('LLLL')}</p>
+    <p>Runtime: {runtime} minutes</p>
+    <p>Rating: {rating.average || 'waiting for more votes'}</p>
+  </EpisodeInfoDiv>
+);
---
+++ src/components/EpisodeInfo/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const EpisodeInfoDiv = styled.div`
+  max-width: 900px;
+  background: #fff;
+  border-radius: 20px;
+  padding: 20px;
+  line-height: 2.5rem;
+  font-size: 1.5rem;
+  margin-top: 5rem;
+  margin-left: 5rem;
+`;
